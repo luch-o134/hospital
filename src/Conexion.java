727246a77ab23eb639ad6396a76e23c6b20055b5
luch-o134 <luiscabaabarca10@gmail.com>
2026-05-25 14:51:47 -0400

    contraseña segura

--- src/Conexion.java
+++ src/Conexion.java
@@ -1,15 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Conexion {
     public static Connection conectar() {
         Connection conexion = null;
         try {
-            String url = "jdbc:postgresql://aws-1-us-east-2.pooler.supabase.com:5432/hospital";
-            String usuario = "postgres.uffawymdecravxievkih";
-            String contraseña = "labcode2026...";
+            Dotenv dotenv = Dotenv.load();
+    
+            String url = dotenv.get("DB_URL");
+            String usuario = dotenv.get("DB_USER");
+            String contraseña = dotenv.get("DB_PASSWORD");
+
             conexion = DriverManager.getConnection(url, usuario, contraseña);
             System.out.println("Conexión exitosa");
+            
         } catch (Exception e) {
             System.out.println("Error de conexión");
             System.out.println(e.getMessage());
